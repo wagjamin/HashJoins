ea49ee935570f8cbdef56e481b36ccb35d4c3601
wagjamin <wagnerbe@in.tum.de>
2018-09-20 17:46:48 +0200

    Proper NOP tests.

--- joins/test/algorithms/nop_join_test.cpp
+++ joins/test/algorithms/nop_join_test.cpp
@@ -68,4 +68,23 @@ TEST(NopTest, CrossTester2) {
     ASSERT_EQ((*join.get()).size(), count*count);
 }
 
+// Statistical test, usually should not fail
+TEST(NopTest, StatisticalTester){
+    uint64_t count = static_cast<uint64_t>(1) << static_cast<uint64_t>(16);
+    uint64_t min = 1;
+    uint64_t max = static_cast<uint64_t>(1) << static_cast<uint64_t>(10);
+    uniform_generator gen(min, max, count);
+    gen.build();
+    auto left = gen.get();
+    gen.build();
+    auto right = gen.get();
+    nop_join join(left, right);
+    join.execute();
+    // Expected overall amount of join partners
+    uint64_t expected = max * (count/max) * (count/max);
+    ASSERT_LE(0.95 * expected, (*join.get()).size());
+    ASSERT_GE(1.05 * expected, (*join.get()).size());
+}
+
+
 
